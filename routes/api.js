314cba142ebb81af4709a3ad2ffc5f4031350c8e
Danganh6333 <134066905+Danganh6333@users.noreply.github.com>
2024-09-28 00:13:59 +0700

    Service và Category

--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,14 @@ const {get_list_banner,addBanner} = require('../controllers/bannerController');
 router.get('/get_list_banner',get_list_banner)
 router.post('/addBanner',addBanner)
 
+//Service API
+router.get('/get_list_service',getListService)
+router.post('/add_service',addService)
+router.put('/update_service/:id',updateService)
 
-
-
+//Category API
+router.get('/get_list_category',getListCategory)
+router.post('/add_category',addCategory)
+router.put('/update_category/:id',updateCategory)
 
 module.exports = router;
