d383ce4c7044dd23cf1e6a9b5e729e6dddac5013
Danganh6333 <134066905+Danganh6333@users.noreply.github.com>
2024-11-23 16:12:36 +0700

    ssss

--- routes/api.js
+++ routes/api.js
@@ -8,17 +8,17 @@ const {SigupUser,loginPhone,getUserDetailById}  = require('../controllers/userCo
 const {add_Category_Product,get_list_Category_Product, getCategoryProduct} = require('../controllers/categoryProductController')
 const Upload = require("../config/upload");
 const {getListService,addService,updateService,getListServiceByCategory,getGroupedServices, changeStatusService} = require('../controllers/serviceController');
-const {getListCategory,addCategory,updateCategory, changeStatusCategoryService, getCategoryService,deleteCategory} = require('../controllers/categoryController');
+const {getListCategory,addCategory,updateCategory, changeStatusCategoryService, getCategoryService} = require('../controllers/categoryController');
 const { createReview, updateReview } = require("../controllers/reviewsControllers");
 const { createBarber, updateBarber,get_list_barber } = require("../controllers/barberController");
-const { createNotification, updateNotification, getNotifications } = require("../controllers/notificationController");
-const {get_list_product,add_product,update_product,get_list_product_by_category,get_product_detail,search_products_by_name, deleteProduct} = require("../controllers/productController")
+const { createNotification, updateNotification } = require("../controllers/notificationController");
+const {get_list_product,add_product,update_product,get_list_product_by_category,get_product_detail,search_products_by_name} = require("../controllers/productController")
 const {get_list_cartItem,add_cartItem,delete_cartItem,update_cartItem} = require("../controllers/cartItemController");
 const {get_user_cart,add_cart,update_cart} = require("../controllers/cartController")
 
 // restful Api notifications
 router.post("/notifications/createNotification",createNotification)
-const { addAppointment, getAppointmentsWithPayments, addAppointmentWithPayment, getAppointmentsByUserId, updateAppointmentStatusToCanceled, updateAppointmentStatusToCanceled_ByZaloPay } = require("../controllers/appointmentControllers");
+const { addAppointment, getAppointmentsWithPayments, addAppointmentWithPayment, getAppointmentsByUserId, updateAppointmentStatusToCanceled, updateAppointmentStatusToCanceled_ByZaloPay, updateAppointmentStatus, getAppointmentsAdmin, updateAppointmentStatusAdmin } = require("../controllers/appointmentControllers");
 
 const { addPayment, updatePayment_Canceled_ById, updatePaymentStatus } = require("../controllers/paymentsController");
 const { addOrderWithPayment, getOrdersByUserId, updateOderStatusToCanceled, updateOderStatusToCanceled_ByZaloPay, getAllOrdersAdmin } = require("../controllers/order_productController");
@@ -59,14 +59,12 @@ router.get('/services/update_status_service/:id',changeStatusService)
 router.get('/categorys/get_list_category',getListCategory)
 router.post('/categorys/add_category',Upload.single("image"),addCategory)
 router.put('/categorys/update_category/:id',Upload.single("image"),updateCategory)
-router.get('/categorys/update_status_category/:id',changeStatusCategoryService)
-router.get("/categorys/get_category/:id", getCategoryService)
 
 
 // RESTful API cho CategoryProduct
 router.get('/categoryProducts/get_list_Category_Product', get_list_Category_Product)
 router.post('/categoryProducts/add_category_product',Upload.single("image"),add_Category_Product)
-
+router.get('/categoryProducts/get_category_product/:id', getCategoryProduct)
 // RESTful API cho Review 
 router.post('/reviews', createReview);
 router.put('/reviews/:id', updateReview);
@@ -77,7 +75,6 @@ router.post('/add_Barbers',Upload.single("image"), createBarber);
 router.put('/Update_Barbers/:id',Upload.single("image"), updateBarber);
 
 // RESTful API cho Notifications
-router.get("/notifications/getNotifications", getNotifications);
 router.post('/notifications', createNotification);
 router.put('/notifications/:id', updateNotification);
 router.get('/getnotifications/:user_id', getNotificationsByUserId);
@@ -89,6 +86,7 @@ router.post('/products/add_product',Upload.single("image"),addProduct)
 router.put('/products/update_product/:id',Upload.single("image"),update_product);
 router.get('/products/get_product_detail/:id',get_product_detail)
 router.get('/products/search_product_by_name',search_products_by_name)
+router.get("/products/update_status_product/:id", updateQuantityProduct)
 
 //Restful API cho cart
 router.get('/carts/get_user_cart',get_user_cart);
