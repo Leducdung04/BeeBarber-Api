7aa44a473bd83622938b8f1719bafd3872800d46
quangduong23780 <duongtqph23780@fpt.edu.vn>
2024-11-25 00:39:22 +0700

    Fix bug status Categories,CategoryProduct

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const { createVoucher, updateVoucher, getVouchers, getValidVouchers, getVoucherB
 const { getUserVouchers, useVoucher } = require('../controllers/uservoucherController');
 const {get_list_banner,addBanner,updateBanner,updateBannerStatus,get_list_banner_ByStatus} = require('../controllers/bannerController');
 const {SigupUser,loginPhone,getUserDetailById, getAllUser, lockupUser}  = require('../controllers/userController');
-const {add_Category_Product,get_list_Category_Product, getCategoryProduct} = require('../controllers/categoryProductController')
+const {add_Category_Product,get_list_Category_Product, getCategoryProduct, changeStatusCategoryProduct} = require('../controllers/categoryProductController')
 const Upload = require("../config/upload");
 const {getListService,addService,updateService,getListServiceByCategory,getGroupedServices, changeStatusService} = require('../controllers/serviceController');
 const {getListCategory,addCategory,updateCategory, changeStatusCategoryService, getCategoryService} = require('../controllers/categoryController');
@@ -61,12 +61,14 @@ router.get('/services/update_status_service/:id',changeStatusService)
 router.get('/categorys/get_list_category',getListCategory)
 router.post('/categorys/add_category',Upload.single("image"),addCategory)
 router.put('/categorys/update_category/:id',Upload.single("image"),updateCategory)
+router.get('/categorys/update_status_category/:id', changeStatusCategoryService)
 
 
 // RESTful API cho CategoryProduct
 router.get('/categoryProducts/get_list_Category_Product', get_list_Category_Product)
 router.post('/categoryProducts/add_category_product',Upload.single("image"),add_Category_Product)
 router.get('/categoryProducts/get_category_product/:id', getCategoryProduct)
+router.get('/categoryProducts/update_status_category_product/:id', changeStatusCategoryProduct)
 // RESTful API cho Review 
 router.post('/reviews', createReview);
 router.put('/reviews/:id', updateReview);
